39d88f2caf54263cf878044ca176ba70ddf3a65b
Harry Lewis <harry@upmind.com>
2022-10-19 19:53:33 +0100

    Update provision request duplicate buttons + add next request button

--- resources/views/provision-request-index.blade.php
+++ resources/views/provision-request-index.blade.php
@@ -89,7 +89,7 @@
                     <td class="collapsing">
                         <div class="ui tiny basic buttons">
                             <a class="ui icon button" href="{{ route('provision-request-show', ['provision_request' => $provision_request]) }}" data-tooltip="View request"><i class="eye icon"></i></a>
-                            <a class="ui icon button" href="{{ route('provision-request-new', ['provision_request_id' => $provision_request->id]) }}" data-tooltip="Duplicate request"><i class="copy icon"></i></a>
+                            <a class="ui icon button" href="{{ route('provision-request-new', ['provision_request_id' => $provision_request->id, 'function_name' => $provision_request->function_name, 'use_result_data' => false]) }}" data-tooltip="Duplicate request"><i class="copy icon"></i></a>
                             <a class="ui icon button" id="provision-request-delete-button-{{ $provision_request->id }}" data-tooltip="Delete request"><i class="trash icon"></i></a>
                         </div>
                     </td>
