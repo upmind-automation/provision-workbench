39b7b5b31372c6aab9ab23d67eddf96994961873
Harry Lewis <harry@upmind.com>
2022-10-27 12:07:30 +0100

    Fix filter component for empty filter value

--- resources/views/components/filter.blade.php
+++ resources/views/components/filter.blade.php
@@ -1,4 +1,4 @@
-@if(Request::get($filter_param) == $filter_value)
+@if(Request::has($filter_param) && Request::get($filter_param) == $filter_value)
     <strong>{{ $slot }}</strong> <a data-tooltip="Remove filter" href="{{ Request::fullUrlWithQuery([$filter_param => '']) }}"><i class="zoom-out icon link black"></i></a>
 @else
     {{ $slot }} <a data-tooltip="Add filter" href="{{ Request::fullUrlWithQuery([$filter_param => $filter_value]) }}"><i class="small zoom-in icon link black"></i></a>
